615021c03c4048de115ffa7c6c0988919156a2c1
Meena-54 <meenakshimanne47@gmail.com>
2025-08-28 03:33:52 -0700

    add

---
+++ add.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+ class AdditionExample {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Ask user for two numbers
+        System.out.print("Enter first number: ");
+        int num1 = input.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = input.nextInt();
+
+        // Add the numbers
+        int sum = num1 + num2;
+
+        // Display the result
+        System.out.println("The sum is: " + sum);
+
+        input.close();
+    }
+}
